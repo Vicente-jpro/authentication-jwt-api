1d69cac7fecec3d30a2ea3e122357f69188c848c
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-04 01:02:07 +0100

    Show as json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def create
     @authentication = AuthenticationJwt.new(params[:data], params[:password])
     debugger
-    render json: {token: @authentication.token, @authentication.decoded_token}
+    render json: {token: @authentication.token, decoded_token: @authentication.decoded_token}
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/show'
-  get 'posts/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts, only: [:create, :index, :show]
 end
