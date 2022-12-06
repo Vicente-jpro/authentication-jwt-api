10831fa2f00bd849baf32b619f1fed6cdeb25c02
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-06 23:27:23 +0100

    Autentication done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,20 @@
 class PostsController < ApplicationController
   
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  before_action :authenticate, except: [ :index ]
+  before_action :authenticate, except: [ :index, :create ]
 
   def create
+    
     password = "12345"
-    exp_payload = { data: info, exp: EXP }
-
+    exp_payload = { data: params[:data], exp: Time.now.to_i + 30 }
+  
     @token = JWT.encode exp_payload, password, 'HS256'
     
-    render json: {token: @token, decoded_token: @decoded_token}
+    render json: { token: @token }
   end
   
   def show
+    render json: {message: "Acessou o show"}
   end
 
   def index
@@ -21,13 +23,15 @@ class PostsController < ApplicationController
   private
     def authenticate
       authenticate_or_request_with_http_token do |token, options|
+  
+        leeway = 30 # seconds
         password = "12345"
-        begin
+        #begin
           @decoded_token = JWT.decode token, password, true, 
-          {exp_leeway: LEEWAY, algorithm: 'HS256' }
-        rescue JWT::ExpiredSignature
-          @decoded_token = "Token has expired"
-        end
+          {exp_leeway: leeway, algorithm: 'HS256' }
+        #rescue JWT::ExpiredSignature
+         # @decoded_token = "Token has expired"
+        #end
        
       end
     end
